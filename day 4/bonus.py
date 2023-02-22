2e4121b784f620761447905dcdc710ed6c935dd5
haitashe <d.chmurec@gmail.com>
2023-02-23 01:06:28 +0200

    new day exercises

--- day 4/bonus.py
+++ day 4/bonus.py
@@ -1,5 +1,13 @@
-filenames = ["1. Raw data.txt", "2. Reports.txt", "3. Presesantion.txt"]
+# filenames = ["1. Raw data.txt", "2. Reports.txt", "3. Presentation.txt"]
+#
+# for filename in filenames:
+#     filename = filename.replace('.', '-', 1)
+#     print(filename)
 
-for filename in filenames:
-    filename = filename.replace('.', '-', 1)
-    print(filename)
+
+# elements = ['a', 'b', 'c']
+# print(elements[1])
+
+elements = ['a', 'b', 'c']
+elements[1] = 'x'
+print(elements)
