8b301afa9f17f91166b1573c1c9c830ac44a6a80
haitashe <d.chmurec@gmail.com>
2023-02-27 22:05:21 +0200

    new day exercises

---
+++ day 5/exercise.py
@@ -0,0 +1,18 @@
+# 1
+
+# filenames = ['document', 'report', 'presentation']
+#
+# for index, files in enumerate(filenames):
+#     row = f"{index}-{files.capitalize()}"
+#     print(row)
+#
+#
+
+# 2
+
+# ips = ['100.122.133.105', '100.122.133.111']
+#
+# ip_index = int(input("Enter the index of the IP you want: "))
+# message = f"You chose {ips[ip_index]}"
+# print(message)
+
